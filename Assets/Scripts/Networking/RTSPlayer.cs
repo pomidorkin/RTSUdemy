422ce2e0722299419b359ad0522aab0d9164b43c
Pomidorkin <pbriuzgin@gmail.com>
2021-07-17 15:53:47 +0900

    UI display fix, building prefab added

--- Assets/Scripts/Networking/RTSPlayer.cs
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -1,15 +1,22 @@
 using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RTSPlayer : NetworkBehaviour
 {
-    [SerializeField] private List<Unit> myUnits = new List<Unit>();
+    private List<Unit> myUnits = new List<Unit>();
+    private List<Building> myBuildings = new List<Building>();
 
     public List<Unit> GetUnits()
     {
         return myUnits;
+    }
+
+    public List<Building> GetBuildings()
+    {
+        return myBuildings;
     }
 
     #region Server
@@ -17,14 +24,35 @@ public class RTSPlayer : NetworkBehaviour
     {
         Unit.ServerOnUnitSpawned += HandleUnitSpawned;
         Unit.ServerOnUnitDespawned += HandleUnitDespawned;
+        Building.ServerOnBuildingSpawned += HandleBuildingSpawned;
+        Building.ServerOnBuildingDespawned += HandleBuildingDespawned;
     }
 
     public override void OnStopServer()
     {
         Unit.ServerOnUnitSpawned -= HandleUnitSpawned;
         Unit.ServerOnUnitDespawned -= HandleUnitDespawned;
+        Building.ServerOnBuildingSpawned -= HandleBuildingSpawned;
+        Building.ServerOnBuildingDespawned -= HandleBuildingDespawned;
     }
-    
+
+
+    private void HandleBuildingSpawned(Building building)
+    {
+        // Checking if the builnging`s owner id is the same with this player`s id
+        if (building.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        myBuildings.Add(building);
+    }
+
+    private void HandleBuildingDespawned(Building building)
+    {
+        if (building.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        myBuildings.Remove(building);
+    }
+
+
     // Since this script is on a plyaer, all players will have this scrips. We don`t want all players
     // to have same Lists of every unit in the game. We want each list to have units for that player
     private void HandleUnitSpawned(Unit unit)
@@ -51,6 +79,8 @@ public class RTSPlayer : NetworkBehaviour
         if (NetworkServer.active) { return; } // Checking if we are the server, if true - return
         Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
         Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
+        Building.AuthorityOnBuildingSpawned += AuthorityHandleBuildingSpawned;
+        Building.AuthorityOnBuildingDespawned += AuthorityHandleBuildingDespawned;
     }
 
     public override void OnStopClient()
@@ -58,6 +88,8 @@ public class RTSPlayer : NetworkBehaviour
         if (!isClientOnly || !hasAuthority) { return; }
         Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
         Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned;
+        Building.AuthorityOnBuildingSpawned -= AuthorityHandleBuildingSpawned;
+        Building.AuthorityOnBuildingDespawned -= AuthorityHandleBuildingDespawned;
     }
 
     private void AuthorityHandleUnitSpawned(Unit unit)
@@ -68,6 +100,16 @@ public class RTSPlayer : NetworkBehaviour
     private void AuthorityHandleUnitDespawned(Unit unit)
     {
         myUnits.Remove(unit);
+    }
+
+    private void AuthorityHandleBuildingSpawned(Building building)
+    {
+        myBuildings.Add(building);
+    }
+
+    private void AuthorityHandleBuildingDespawned(Building building)
+    {
+        myBuildings.Remove(building);
     }
 
     #endregion
